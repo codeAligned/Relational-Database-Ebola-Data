e8d6dfe62f0b7f87c49ace663e7e40affeabd5fd
j-chao <justin_chao@utexas.edu>
2016-09-24 20:52:50 -0500

    renamed attributes for better clarity

--- lab1/create_tables.sql
+++ lab1/create_tables.sql
@@ -19,56 +19,56 @@ set default_storage_engine = InnoDB;
 select CONCAT('storage engine: ', @@default_storage_engine) as INFO;
 
 CREATE TABLE Country (
-    country_name    VARCHAR(20)     NOT NULL,
-    health_exp      INTEGER         NOT NULL,
-    gdp_cap         INTEGER         NOT NULL,
-    urban_pop       INTEGER         NOT NULL,
+    country_name        VARCHAR(20)     NOT NULL,
+    health_exp          INTEGER         NOT NULL,
+    gdp_cap             INTEGER         NOT NULL,
+    urban_pop           INTEGER         NOT NULL,
     PRIMARY KEY (country_name)
 );
 
 CREATE TABLE ETC (
-    etc_code        CHAR(8)         NOT NULL,
-    country_name    VARCHAR(20)     NOT NULL,
-    partner_org     VARCHAR(20),
-    etc_name        VARCHAR(20)     NOT NULL,
-    latitude        double          NOT NULL,
-    longitude       double          NOT NULL,
-    lab_present     CHAR(1)         NOT NULL,
-    status          VARCHAR(20)     NOT NULL,
-    beds_open       INTEGER         NOT NULL,
+    etc_code            CHAR(8)         NOT NULL,
+    country_name        VARCHAR(20)     NOT NULL,
+    partner_org         VARCHAR(20),
+    etc_name            VARCHAR(20)     NOT NULL,
+    latitude            double          NOT NULL,
+    longitude           double          NOT NULL,
+    lab_present         CHAR(1)         NOT NULL,
+    status              VARCHAR(20)     NOT NULL,
+    beds_open           INTEGER         NOT NULL,
     PRIMARY KEY (etc_code),
     FOREIGN KEY (country_name)  REFERENCES Country (country_name)    ON DELETE CASCADE
 );
 
 CREATE TABLE Survey_Respondent (
-    respid          INTEGER         NOT NULL,
-    country_name    VARCHAR(20)     NOT NULL,
-    gender          CHAR(1)         NOT NULL,
-    age             INTEGER         NOT NULL,
-    education       INTEGER         NOT NULL,
-    corganizedae    INTEGER         NOT NULL,
+    respid              INTEGER         NOT NULL,
+    country_name        VARCHAR(20)     NOT NULL,
+    gender              CHAR(1)         NOT NULL,
+    age                 INTEGER         NOT NULL,
+    education           INTEGER         NOT NULL,
+    corganizedae        INTEGER         NOT NULL,
     PRIMARY KEY (respid),
     FOREIGN KEY (country_name)  REFERENCES Country (country_name)    ON DELETE CASCADE
 );
 
 CREATE TABLE Organization (
-    acronym         VARCHAR(20)     NOT NULL,
-    country_name    VARCHAR(20)     NOT NULL,
-    etc_code        CHAR(8)         NOT NULL,
-    org_name        VARCHAR(50)     NOT NULL,
-    org_type        VARCHAR(20)     NOT NULL,
-    PRIMARY KEY (acronym),
+    org_acronym         VARCHAR(20)     NOT NULL,
+    country_name        VARCHAR(20)     NOT NULL,
+    etc_code            CHAR(8)         NOT NULL,
+    org_name            VARCHAR(50)     NOT NULL,
+    org_type            VARCHAR(20)     NOT NULL,
+    PRIMARY KEY (org_acronym),
     FOREIGN KEY (country_name)  REFERENCES Country (country_name)    ON DELETE CASCADE
 );
 
 CREATE TABLE ETC_Org (
     etc_code        CHAR(8)         NOT NULL,
-    acronym         VARCHAR(20)     NOT NULL,
+    org_acronym     VARCHAR(20)     NOT NULL,
     CONSTRAINT PK_ETC_Org PRIMARY KEY
     (
         etc_code,
-        acronym
+        org_acronym
     ),
     FOREIGN KEY (etc_code) REFERENCES ETC (etc_code),
-    FOREIGN KEY (acronym) REFERENCES Organization (acronym)
+    FOREIGN KEY (org_acronym) REFERENCES Organization (org_acronym)
 );
