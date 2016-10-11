023abd19758c94f0a09522ebf698b7d3e5bee5f2
j-chao <justin_chao@utexas.edu>
2016-10-11 09:05:35 -0500

    moved import scripts to import_script dir

--- lab2/import_script/import_Country.py
+++ lab2/import_script/import_Country.py
@@ -1,19 +1,60 @@
-#!/usr/local/bin/python
+# Import Country Table
+
 import pymysql
+import csv
 
-db = pymysql.connect (host   ="localhost", # your host, for this lab, you don't need to change
-                      user   = "root",     # your username
-                      passwd = "",         # your password
-                      db     = "ebola")    # name of the database
+def create_connection():
+    try:
+        connection = pymysql.connect(host="127.0.0.1",
+                                     user="root",
+                                     passwd="",
+                                     db="Ebola")
+        return connection
+    
+    except pymysql.Error as error:
+        print ("connection error: ", error)
 
-# you must create a Cursor object. It will let you execute all the queries you need
-cur = db.cursor()
 
-# Use all the SQL you like
+def run_insert(insert_stmt):
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(insert_stmt)
+        conn.commit()
+        destroy_connection(conn)
+
+    except pymysql.Error as error:
+        print ("insert error: ", error)
 
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row
+def import_csv():
+    insert_prefix = "insert into Country (country_name, urban_pop, health_exp, gdp_cap) values ("
+    try:
+        csvfile = open("../Datasets/CIA_World_Factbook.csv", "rb")
+        reader = csv.reader(csvfile)
+        for i, row in enumerate(reader):
+            if i==0: continue                           # skip column names row      
+            insert_stmt = insert_prefix
+            
+            for j, val in enumerate(row):
+                if j==0:
+                    insert_stmt += "'" + val + "', "
+                elif j==1 or j==2:                      # handles numeric types
+                    insert_stmt += val + ", "
+                else:                                   # handles last/numeric value
+                    insert_stmt += val 
+            insert_stmt += ");"
+            print(insert_stmt)
+            run_insert(insert_stmt)
+                
+    except IOError as e:
+        print ("IO Error: " + e.strerror)
 
-db.close()
+
+def destroy_connection(conn):
+    conn.close()
+
+
+def main():
+    import_csv()
+main()
