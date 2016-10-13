ddaaab6e6133886c7ad30fbd65b6e5b5e13bd8d5
Julianne Crea <juliannecrea@Juliannes-MacBook-Pro.local>
2016-10-13 00:01:34 -0500

    made import_ETC & import_Country & import_Partner_Org all work without duplicating pks

--- lab2/import_script/import_Partner_Org_ETC.py
+++ lab2/import_script/import_Partner_Org_ETC.py
@@ -8,30 +8,58 @@ def import_csv():
     insert_prefix = "INSERT INTO Partner_Org_ETC (etc_code, partner_org) VALUES ("
 
     try:
-        csvfile = open("../Datasets/ETC.csv", "rb")
-        reader = csv.reader(csvfile)
+        partners_used = list()
+        rows = list(csv.reader(open('../Datasets/ETC.csv', 'rb'), delimiter=","))
+        rows.remove(rows[0])
+        unique_rows = list()
+        for row in rows:
+            partner = row[4]
+            if partner not in partners_used:
+                partners_used.append(row[4])
+                unique_rows.append(row)
 
-        for i, row in enumerate(reader):
-            if i==0: continue                           # skip column names row      
+        for i, row in enumerate(unique_rows):
             insert_stmt = insert_prefix
             
             for j, val in enumerate(row):
                 if val:
                     if j==0:
                         insert_stmt += "'" + val + "', "
-                    elif j==4:                          # handles numeric types
+                    elif j==4:
                         insert_stmt += "'" + val + "'"
-                    else:                                   # handles last value
+                    else:
                         continue
+                else:
+                    if j==4:
+                        insert_stmt += "NULL"
+                    elif j==0:
+                        insert_stmt += "NULL" + ", "
+
+            insert_stmt += ");"
+            print(insert_stmt)
+                    
+
+     #   for i, row in enumerate(reader):
+#            if i==0: continue                           # skip column names row      
+#            insert_stmt = insert_prefix
+            
+#            for j, val in enumerate(row):
+#                if val:
+#                    if j==0:
+#                        insert_stmt += "'" + val + "', "
+#                    elif j==4:                          # handles numeric types
+#                        insert_stmt += "'" + val + "'"
+#                    else:                                   # handles last value
+#                        continue
  #               else:
 #                    if j == 4:
 #                        insert_stmt += "NULL"
 #                    else:
 #                        insert_stmt += "Null" + ", "
-            insert_stmt += ");"
-            if i==4:
-                print(insert_stmt)
-            run_insert(insert_stmt)
+ #           insert_stmt += ");"
+#            if i==4:
+#                print(insert_stmt)
+#            run_insert(insert_stmt)
                 
     except IOError as e:
         print ("IO Error: " + e.strerror)
--- lab2/import_script/imports_Partner_Orgs.py
+++ lab2/import_script/imports_Partner_Orgs.py
@@ -8,22 +8,32 @@ def import_csv():
     insert_prefix = "INSERT INTO Partner_Orgs (partner_org) VALUES ("
 
     try:
-        csvfile = open("../Datasets/ETC.csv", "rb")
-        reader = csv.reader(csvfile)
+        partners_used = list()
+        rows = list(csv.reader(open('../Datasets/ETC.csv', 'rb'), delimiter=","))
+        rows.remove(rows[0])
+        unique_rows = list()
+        for row in rows:
+            partner = row[4]
+            if len(partner) == 0:
+                partners_used.append(partner)
+            if partner not in partners_used:
+                partners_used.append(row[4])
+                unique_rows.append(row)
 
-        
-        for i, row in enumerate(reader):
-            if i==0: continue                           # skip column names row      
+        for i, row in enumerate(unique_rows):
             insert_stmt = insert_prefix
             
             for j, val in enumerate(row):
                 if val:
                     if j==4:
                         insert_stmt += "'" + val + "'"
-                    else:                                   
+                    else:
                         continue
 
+
             insert_stmt += ");"
+            print(insert_stmt)
+                    
 
             run_insert(insert_stmt)
                 
