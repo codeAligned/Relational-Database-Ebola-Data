056fb007dc8ecc262c0ed6a17561da50bdc1f3e1
Julianne Crea <juliannecrea@Juliannes-MacBook-Pro.local>
2016-10-12 22:06:49 -0500

    removed partner_org from ETC table; created partner_org_ETC table (one to many, should be many to many?)

--- lab2/import_script/import_Partner_Org_ETC.py
+++ lab2/import_script/import_Partner_Org_ETC.py
@@ -1,11 +1,11 @@
-# Import ETC from ETC.csv
+# Import Partner_Org_ETC from ETC.csv
 
 import pymysql
 import csv
 from db_connect import *
 
 def import_csv():
-    insert_prefix = "INSERT INTO ETC (etc_code,	etc_name, status, beds_open, latitude, longitude, lab_present, country_name) VALUES ("
+    insert_prefix = "INSERT INTO Partner_Org_ETC (etc_code, partner_org) VALUES ("
 
     try:
         csvfile = open("../Datasets/ETC.csv", "rb")
@@ -16,21 +16,21 @@ def import_csv():
             insert_stmt = insert_prefix
             
             for j, val in enumerate(row):
-                if j==4: continue
                 if val:
-                    if j==0 or j==1 or j==2 or j==7:
+                    if j==0:
                         insert_stmt += "'" + val + "', "
-                    elif j==3 or j==5 or j==6:              # handles numeric types
-                        insert_stmt += val + ", "
-                    else:                                   # handles last value
+                    elif j==4:                          # handles numeric types
                         insert_stmt += "'" + val + "'"
-                else:
-                    if j == 8:
-                        insert_stmt += "NULL"
-                    else:
-                        insert_stmt += "Null" + ", "
+                    else:                                   # handles last value
+                        continue
+ #               else:
+#                    if j == 4:
+#                        insert_stmt += "NULL"
+#                    else:
+#                        insert_stmt += "Null" + ", "
             insert_stmt += ");"
-
+            if i==4:
+                print(insert_stmt)
             run_insert(insert_stmt)
                 
     except IOError as e:
