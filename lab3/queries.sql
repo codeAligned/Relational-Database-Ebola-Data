39c916cb86567dbeb4c7d41db99a16292c21a65a
Julianne Crea <juliannecrea@juliannes-mbp.attlocal.net>
2016-11-13 17:41:56 -0600

    added 2 more queries

--- lab3/queries.sql
+++ lab3/queries.sql
@@ -1,3 +1,15 @@
 -- SQL Queries
 
-SELECT* FROM ETC WHERE country_name = 'Guinea' HAVING beds_open>50;
+SELECT * FROM ETC WHERE country_name = ['Guinea'] HAVING beds_open > [50];
+
+
+SELECT AVG(age) as avg_age, AVG(education) as avg_education 
+FROM survey_respondent 
+WHERE gender = ['M'] 
+AND country_name = ['liberia'] 
+AND (education != 88 OR education != NULL);
+
+SELECT COUNT(education) as selected_educ_or_higher
+FROM survey_respondent
+WHERE education >= [6]
+AND GENDER = ['F'];
