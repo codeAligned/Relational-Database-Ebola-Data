87e844db0dc0a47be42f1ce21e4be84d32e841f0
j-chao <justin_chao@utexas.edu>
2016-11-14 14:49:49 -0600

    added queries by created views

--- lab3/queries.sql
+++ lab3/queries.sql
@@ -1,4 +1,4 @@
--- SQL Queries
+-- SQL Create Views and Queries for Ebola Database
 
 -------------------------- VIEWS --------------------------
 -- create view from Survey_Respondent of gender, age, education, country_name
@@ -17,7 +17,8 @@ FROM ETC, Partner_Orgs;
 ------------------------- QUERIES -------------------------
 
 -- show ETCs in a selected country that have greater than a selected number of open beds
-SELECT * FROM ETC 
+SELECT * 
+FROM ETC 
 WHERE country_name = ['Guinea'] 
 HAVING beds_open > [50];
 
@@ -36,17 +37,23 @@ FROM survey_respondent
 WHERE education >= [6]
 AND GENDER = ['F'];
 
-
 -- show Partner Orgs with their longitude/latitude coordinates
+-- where coordinates are given
 SELECT partner_org, latitude, longitude  
 FROM Partner_orgs INNER JOIN ETC 
 WHERE (latitude != 0 OR longitude != 0);
 
 -- show ETCs by etc_code that selected Organization is working with
-SELECT org_name,  etc_code  
+SELECT org_name, etc_code  
 FROM Country_Org INNER JOIN ETC 
 WHERE org_name = ['UNMEER'];
 
 
+--------------------- QUERIES BY VIEWS ---------------------
+
+-- show surveyresp_country, ordered by increasing age of survey respondents
+SELECT * 
+FROM surveyresp_country 
+ORDER BY age;
 
 
