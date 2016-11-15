e5a8b26f3927753b61873f71458809ee29ece1ae
Julianne Crea <juliannecrea@Juliannes-MacBook-Pro.local>
2016-11-14 23:15:51 -0600

    query interface

--- lab3/query_interface.py
+++ lab3/query_interface.py
@@ -4,17 +4,16 @@ from db_connect import *
 
 def print_menu():
     print ('Query options:')
-    print ('1: List ETCs in Guinea having greater than 50 open beds.')
-    print ('2: List average age and education level of males in Liberia.')
-    print ('3: Count number of female survey respondents that have at least '
-            'an education level of 6.')
-    print ('4: ')
-    print ('5: ')
-    print ('6: ')
-    print ('7: ')
-    print ('8: ')
-    print ('9: ')
-    print ('10: ')
+    print ('1:  List ETCs in the country __ having more than __ open beds.')
+    print ('2:  List average age & education of respondents whose sex=__ and live in __.')
+    print ('3:  Count the respondents whose sex=__ and who have at least an education of __.')
+    print ('4:  Display partner organizations with their longitude/latitude coordinates.')
+    print ('5:  Display a chosen organization __ & codes of the ETCs it is working with.')
+    print ('6:  Display all distinct organization types.')
+    print ('7:  Display every respondent\'s (whose sex=__) info & their country\'s info.')
+    print ('8:  Display every ETC that isn\'t closed & its info/partner organization.')
+    print ('9:  List countries in ascending order by GDP.')
+    print ('10: Count the respondents (with sex=__, education>=__, and country=__) who think their community was well organized.')
     print ''
 
 def run_query_case():
@@ -22,23 +21,57 @@ def run_query_case():
     if (case == 1):
         etc_open_beds()
     elif (case == 2):
-        age_edu_male_liberia()
+        age_edu_sex_country()
     elif (case == 3):
-        count_female_edu6()
+        count_sex_educ()
     elif (case == 4):
-        age_edu_male_liberia()
+        partner_lat_long()
     elif (case == 5):
-        age_edu_male_liberia()
+        org_ETC_codes()
     elif (case == 6):
-        age_edu_male_liberia()
+        distinct_org_types()
     elif (case == 7):
-        age_edu_male_liberia()
+        respondent_country_info()
     elif (case == 8):
-        age_edu_male_liberia()
+        non_closed_ETC_partner()
     elif (case == 9):
-        age_edu_male_liberia()
+        country_gdp()
     elif (case == 10):
-        age_edu_male_liberia()
+        count_organized()
+    else:
+        print("Sorry, that is not an option.")
+
+# Ten queries functions
+def etc_open_beds(): # needs user input on country, beds_open number
+    stmt = 'SELECT * FROM ETC WHERE country_name = \'Guinea\' HAVING beds_open > 50 ORDER BY beds_open;'
+    run_insert(stmt)
+def age_edu_sex_country(): # needs user input on gender, country
+    stmt = 'SELECT AVG(age) as avg_age, AVG(education) as avg_education FROM survey_respondent WHERE gender = \'M\' AND country_name = \'liberia\' AND (education != 88 OR education != NULL);'
+    run_insert(stmt)
+def count_sex_educ(): # needs user input on education, gender
+    stmt = 'SELECT COUNT(education) as selected_educ_or_higher FROM survey_respondent WHERE education >= 6 AND GENDER = \'F\';'
+    run_insert(stmt)
+def partner_lat_long():
+    stmt = 'SELECT partner_org, latitude, longitude FROM Partner_orgs INNER JOIN ETC WHERE (latitude != 0 OR longitude != 0);'
+    run_insert(stmt)
+def org_ETC_codes(): # needs user input on org_name
+    stmt = 'SELECT org_name, etc_code FROM Country_Org INNER JOIN ETC WHERE org_name = \'UNMEER\' ORDER BY etc_code;'
+    run_insert(stmt)
+def distinct_org_types():
+    stmt = 'SELECT DISTINCT org_type FROM Organization;'
+    run_insert(stmt)
+def respondent_country_info(): # needs user input on gender
+    stmt = 'SELECT * FROM Country LEFT OUTER JOIN Survey_Respondent on Country.country_name = Survey_Respondent.country_name WHERE gender = \'M\';'
+    run_insert(stmt)
+def non_closed_ETC_partner():
+    stmt = 'SELECT * FROM ETC LEFT OUTER JOIN Partner_org_ETC on ETC.etc_code=Partner_org_ETC.etc_code WHERE status=\'Open\' or status=\'Under Construction\';'
+    run_insert(stmt)
+def country_gdp():
+    stmt = 'SELECT * FROM Country ORDER BY gdp_cap;'
+    run_insert(stmt)
+def count_organized(): # needs used input for gender, educ, countryName
+    stmt = 'SELECT COUNT(corganizedae) as count_organized FROM Survey_Respondent WHERE gender = \'M\' AND corganizedae = 1 AND education >= 3 AND country_name = \'Liberia\';'
+    run_insert(stmt)
 
 # Functions to create views
 def create_view_SurveyResp_Country():
@@ -65,7 +98,6 @@ def count_female_edu6():
 
 # run_insert error checking
 # ...
-
 
 
 def main():
