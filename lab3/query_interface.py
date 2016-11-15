fd91145665a76be05c5e8665297bfceb785e0f12
j-chao <justin_chao@utexas.edu>
2016-11-14 18:04:55 -0600

    added query case 3

--- lab3/query_interface.py
+++ lab3/query_interface.py
@@ -17,6 +17,30 @@ def print_menu():
     print ('10: ')
     print ''
 
+def run_query_case():
+    case = int(input('Enter query option number: '))
+    if (case == 1):
+        etc_open_beds()
+    elif (case == 2):
+        age_edu_male_liberia()
+    elif (case == 3):
+        count_female_edu6()
+    elif (case == 4):
+        age_edu_male_liberia()
+    elif (case == 5):
+        age_edu_male_liberia()
+    elif (case == 6):
+        age_edu_male_liberia()
+    elif (case == 7):
+        age_edu_male_liberia()
+    elif (case == 8):
+        age_edu_male_liberia()
+    elif (case == 9):
+        age_edu_male_liberia()
+    elif (case == 10):
+        age_edu_male_liberia()
+
+# Functions to create views
 def create_view_SurveyResp_Country():
     stmt = 'CREATE VIEW SurveyResp_Country (gender, age, education, country_name) AS SELECT gender, age, education, country_name FROM Survey_Respondent;'
     run_insert(stmt)
@@ -25,6 +49,7 @@ def create_view_etc_limited():
     stmt = 'CREATE VIEW ETC_limited (etc_name, country_name, partner_org) AS SELECT etc_name, country_name, partner_orgFROM ETC, Partner_Orgs;'
     run_insert(stmt)
 
+# Functions to run queries
 def etc_open_beds():
     stmt = "SELECT * FROM ETC WHERE country_name = 'Guinea' HAVING beds_open > 50 ORDER BY beds_open;"
     run_insert(stmt)
@@ -32,6 +57,15 @@ def etc_open_beds():
 def age_edu_male_liberia():
     stmt = "SELECT AVG(age) as avg_age, AVG(education) as avg_education FROM survey_respondent WHERE gender = 'M' AND country_name = 'liberia' AND (education != 88 OR education != NULL);"
     run_insert(stmt)
+
+def count_female_edu6():
+    stmt = "SELECT COUNT(education) AS number_females FROM survey_respondent WHERE education >= 6 AND GENDER = 'F';"
+    run_insert(stmt)
+
+
+# run_insert error checking
+# ...
+
 
 
 def main():
@@ -47,13 +81,7 @@ def main():
     print_menu()
     
     # prompt user for query option
-    case = int(input('Enter query option number: '))
-    if (case == 1):
-        etc_open_beds()
-    elif (case == 2):
-        age_edu_male_liberia()
-
-
+    run_query_case()
 
 
 
